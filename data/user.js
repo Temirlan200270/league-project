3b6215fbcd203fa9a606cb4ef676a5ed45f73c1d
Temirlan200270 <135714714+Temirlan200270@users.noreply.github.com>
2025-02-14 15:59:01 +0600

    Add files via upload

---
+++ data/user.js
@@ -0,0 +1,13 @@
+const userData = {
+    name: "Test User",
+    id: "12345",
+    level: 30,
+    avatarUrl: "pictures/avatar.jpg", // Путь к вашей картинке
+    stats: {
+        wins: 210,
+        losses: 120,
+        winrate: "63.64%",
+        bestChampion: "Yasuo",
+        bestSeason: "Gold"
+    }
+};
